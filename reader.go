5521965d02be777bcbd06c73d7f428ced4659af0
niraj-khatiwada <nirazzz.nk@gmail.com>
2023-11-01 21:45:06 +0545

    go basics

---
+++ reader.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	fmt.Println("What's your name brother? ")
+	var reader = bufio.NewReader(os.Stdin)
+	var str, err = reader.ReadString('\n')
+	if err != nil {
+		panic("Oops!")
+	}
+	fmt.Println("Howdy", str)
+}
