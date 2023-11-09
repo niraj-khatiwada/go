c5858091abc027d5797087c39d5dfed4bb43ced2
niraj-khatiwada <nirazzz.nk@gmail.com>
2023-11-09 21:24:16 +0545

    errors.Is()

--- main.go
+++ main.go
@@ -10,6 +10,10 @@ var targetError = errors.New("error not found")
 func main() {
 	err := throwsError()
 	fmt.Println(errors.Is(err, targetError))
+
+	hashMap := map[string]int{"niraj": 2}
+
+	fmt.Println(len(hashMap))
 }
 
 func throwsError() error {
