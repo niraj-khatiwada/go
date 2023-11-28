d005ff36e8de1efd31a21a20671ebdf5c1e5da34
niraj-khatiwada <nirazzz.nk@gmail.com>
2023-11-28 13:55:57 +0545

    rest and spread operators

--- main.go
+++ main.go
@@ -1,42 +1,15 @@
 package main
 
-import (
-	"fmt"
-	"math/rand"
-	"sync"
-	"time"
-)
-
-type Account struct {
-	balance int
-	lock    sync.Mutex
-}
-
-var account = Account{balance: 200}
+import "fmt"
 
 func main() {
-	go (func() {
-		if err := withdraw(&account, 100); err != nil {
-			fmt.Println(err)
-		}
-	})()
-	go (func() {
-		if err := withdraw(&account, 150); err != nil {
-			fmt.Println(err)
-		}
-	})()
-	time.Sleep(1 * time.Second)
-}
+	rest := getNames("Niraj", "Suraj")
+	fmt.Println(rest)
 
-func withdraw(account *Account, amount int) error {
-	account.lock.Lock()
-	if amount > account.balance {
-		return fmt.Errorf("[error] withdraw amount greater than the balance")
-	}
-	random := rand.Intn(10)
-	time.Sleep(time.Duration(random) * time.Millisecond)
-	account.balance -= amount
-	account.lock.Unlock()
-	fmt.Printf("\n%d withdrawn. Remaining amount = %d\n", amount, account.balance)
-	return nil
+	//	Spread
+	getNames(rest...)
+}
+func getNames(names ...string) []string {
+	fmt.Println("Rest Names", names)
+	return names
 }
---
+++ spread-rest.go
@@ -0,0 +1,15 @@
+package main
+
+//
+//import "fmt"
+//
+//func main() {
+//	rest := getNames("Niraj", "Suraj")
+//	fmt.Println(rest)
+//
+//	//	Spread
+//	getNames(rest...)
+//}
+//func getNames(names ...string) []string {
+//	return names
+//}
